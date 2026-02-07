cbcad67dc4ce455431e1ef3bc20f18ef15319e6c
JKumar212 <36420419+JKumar212@users.noreply.github.com>
2026-02-07 09:17:06 +0530

    Change SPREADSHEET_ID in config.js
    Updated SPREADSHEET_ID to a new value.

--- js/config.js
+++ js/config.js
@@ -11,7 +11,8 @@ const CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbxHfMS_CeHzNckTAJd40ZBiuf_U0H0n_-Y9-eE0P6n55WtOmU33eb7paN20_RXEi67K/execE',
   
   // Your Google Sheets and Drive IDs (already configured in backend)
-  SPREADSHEET_ID: '1kXdNZKnqDDu800M3KWT0yhFa4w8ARHjs7ji435isEDU',
+  //SPREADSHEET_ID: '1kXdNZKnqDDu800M3KWT0yhFa4w8ARHjs7ji435isEDU',
+  SPREADSHEET_ID: '13lGXsSzzbJ1_GpiuOS1jvzUXOuejx5eqd5sJPDbB5-I',
   DRIVE_FOLDER_ID: '12PYR4pIxoxCQjAaL1s_cIDgjXN8TD4vt',
   
   // API timeout (milliseconds)
